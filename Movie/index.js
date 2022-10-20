e6a0f51e7309fc8af1c0a79c5b775b1e4c8ca4af
cansualtun <altuncansu98@gmail.com>
2022-10-20 22:48:56 +0300

    movieappstart

---
+++ Movie/index.js
@@ -0,0 +1,10 @@
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "2797bfdf",
+      s: "avengers",
+    },
+  });
+  console.log(response.data);
+};
+fetchData();
